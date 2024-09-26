dc039648c16e0c0993c4c06e3087accdead454e8
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-09-25 21:35:39 -0400

    updated socket with deployment location

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://chatapp-mura.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
